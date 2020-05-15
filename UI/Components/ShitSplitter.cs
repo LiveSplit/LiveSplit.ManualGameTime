6e4ed71b6c4b3eece1adf18ce0093610b0e86315
wooferzfg <spapushin@gmail.com>
2020-05-14 21:01:33 -0500

    Suppress sound when pressing the enter key

--- UI/Components/ShitSplitter.cs
+++ UI/Components/ShitSplitter.cs
@@ -39,6 +39,8 @@ namespace LiveSplit.ManualGameTime.UI.Components
                     txtGameTime.Text = "";
                 }
                 catch { }
+
+                e.Handled = true;
             }
         }
     }
