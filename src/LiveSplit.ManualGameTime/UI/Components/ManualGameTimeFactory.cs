a343a2174e1a6f769893dfe58e248d23cd3b4f70
wooferzfg <spapushin@gmail.com>
2024-05-11 09:48:16 -0400

    Update version to 1.8.29

--- src/LiveSplit.ManualGameTime/UI/Components/ManualGameTimeFactory.cs
+++ src/LiveSplit.ManualGameTime/UI/Components/ManualGameTimeFactory.cs
@@ -23,6 +23,6 @@ namespace LiveSplit.Delta
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.9");
+        public Version Version => Version.Parse("1.8.29");
     }
 }
