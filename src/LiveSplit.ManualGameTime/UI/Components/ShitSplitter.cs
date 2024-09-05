65b1ee4f18615c52e43d7ce41dba96b3c0d9ad86
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:22 +0200

    Add `.editorconfig` & apply style rules (#7)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Add and order modifiers
    
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.ManualGameTime/UI/Components/ShitSplitter.cs
+++ src/LiveSplit.ManualGameTime/UI/Components/ShitSplitter.cs
@@ -1,47 +1,47 @@
-﻿using LiveSplit.Model;
-using LiveSplit.UI.Components;
-using System;
+﻿using System;
 using System.Windows.Forms;
 
-namespace LiveSplit.ManualGameTime.UI.Components
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+namespace LiveSplit.ManualGameTime.UI.Components;
+
+public partial class ShitSplitter : Form
 {
-    public partial class ShitSplitter : Form
+    protected ITimerModel Model { get; set; }
+    protected ManualGameTimeSettings Settings { get; set; }
+
+    public ShitSplitter(LiveSplitState state, ManualGameTimeSettings settings)
     {
-        protected ITimerModel Model { get; set; }
-        protected ManualGameTimeSettings Settings { get; set; }
-
-        public ShitSplitter(LiveSplitState state, ManualGameTimeSettings settings)
+        InitializeComponent();
+        Model = new TimerModel()
         {
-            InitializeComponent();
-            Model = new TimerModel()
-            {
-                CurrentState = state
-            };
-            Settings = settings;
-        }
+            CurrentState = state
+        };
+        Settings = settings;
+    }
 
-        private void txtGameTime_KeyPress(object sender, KeyPressEventArgs e)
+    private void txtGameTime_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == '\r')
         {
-            if (e.KeyChar == '\r')
+            try
             {
-                try
+                string[] timeSpans = txtGameTime.Text.Replace(" ", "").Split('+');
+                TimeSpan totalTime = TimeSpan.Zero;
+                foreach (string time in timeSpans)
                 {
-                    var timeSpans = txtGameTime.Text.Replace(" ", "").Split('+');
-                    var totalTime = TimeSpan.Zero;
-                    foreach (var time in timeSpans)
-                    {
-                        totalTime += TimeSpanParser.Parse(time);
-                    }
-
-                    var newGameTime = totalTime + (Settings.UseSegmentTimes ? Model.CurrentState.CurrentTime.GameTime : TimeSpan.Zero);
-                    Model.CurrentState.SetGameTime(newGameTime);
-                    Model.Split();
-                    txtGameTime.Text = "";
+                    totalTime += TimeSpanParser.Parse(time);
                 }
-                catch { }
 
-                e.Handled = true;
+                TimeSpan? newGameTime = totalTime + (Settings.UseSegmentTimes ? Model.CurrentState.CurrentTime.GameTime : TimeSpan.Zero);
+                Model.CurrentState.SetGameTime(newGameTime);
+                Model.Split();
+                txtGameTime.Text = "";
             }
+            catch { }
+
+            e.Handled = true;
         }
     }
 }
